9826a344361298dab8342cafd9ae580a51fb8937
Anand raja <meetanandns@gmail.com>
2022-01-14 09:58:55 +0000

    basic working app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <ul>
-    <learning-resource v-for="res in storedResources" :key="res.id"
-    :title="res.title" :description="res.description" :link="res.link"
-    ></learning-resource>
-  </ul>
+  <the-header :title="title"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue'; 
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
-    LearningResource
+    StoredResources,
+    TheHeader
   },
   data() {
     return {
@@ -28,8 +27,25 @@ export default {
           description: 'This is a Google official page/',
           link: 'https://www.google.com/'
         }
-      ]
+      ],
+      title: 'Remember Me'
     };
   }
 }
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+</style>
