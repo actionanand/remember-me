6a1f7771c1a736f98910071b1aa42318c93c5cc3
Anand raja <meetanandns@gmail.com>
2022-01-14 07:37:33 +0000

    testing code

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resource v-for="res in storedResources" :key="res.id"
+    :title="res.title" :description="res.description" :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'zaq098jhddb',
+          title: 'Official Guide',
+          description: 'This is Vue Js official description.',
+          link: 'https://v3.vuejs.org/'
+        },
+        {
+          id: 'sgssbvxg754sg',
+          title: 'Google',
+          description: 'This is a Google official page/',
+          link: 'https://www.google.com/'
+        }
+      ]
+    };
+  }
+}
+</script>
